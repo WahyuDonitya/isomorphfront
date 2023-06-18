51c5c0781a8013116e9555b7519d7b8fecf37f54
Reptra <putra.reza111@gmail.com>
2023-06-18 20:52:13 +0700

    Approval team untuk admin
    cobaen yo

--- src/pages/Backend/Admin/Heroes.jsx
+++ src/pages/Backend/Admin/Heroes.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeroesForm from '../../../components/Heroes/HeroesForm'
 import { Container, Typography } from '@mui/material'
+import axios from 'axios'
 
 const Heroes = () => {
   return (
@@ -10,5 +11,35 @@ const Heroes = () => {
     </Container>
   )
 }
+
+const ApprovalTeam = () => {
+  const [approvalsTeam, setApprovalTeam] = useState([])
+}
+
+useEffect(() => {
+  const fetchTeam = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/team");
+      setApprovalTeam(response.data);
+    } catch (error) {
+      console.error("Error fetching options:", error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+return (
+  <div>
+    <h2>Daftar Persetujuan Team</h2>
+    {approvals.map(approval => (
+      <div key={approval._id}>
+        <h3>{approval.name}</h3>
+        <p>Teeam: {approval.value}</p>
+        <p>Status: {approval.status}</p>
+      </div>
+    ))}
+  </div>
+);
 
 export default Heroes
